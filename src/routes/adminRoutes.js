c0e5a08b66de1685b35d6ee22095fb2a21e8ab05
NLIPPI <nlippilira@gmail.com>
2026-03-02 00:29:28 -0300

    ds

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -144,7 +144,7 @@ router.get('/proyectos', (req, res) => {
 })
 
 router.get('/cv', cvController.index)
-router.post('/admin/cv/upload', upload.single('cv'), cvController.uploadCV);
+router.post('/admin/cv/upload', upload.single('cv'), cvController.upload);
 router.post('/cv/delete', cvController.delete)
 
 
